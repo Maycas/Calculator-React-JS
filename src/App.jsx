8a0e02e11b39710788d7df0f689156fcfe4368c8
Marc Maycas <marc.maycas@gmail.com>
2023-04-17 15:13:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.App}>
+      <h1>My React Calculator</h1>
+    </div>
+  )
+}
+
+export default App
